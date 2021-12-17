f88262c2bc3bd5865569ce86e2055fdf5d2825d3
Andrea Righi <andrea.righi@canonical.com>
2021-12-17 09:21:46 +0100

    cycle_scenes.py: add a property called 'enabled'
    Add a property to enable/disable the script at runtime.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- cycle_scenes.py
+++ cycle_scenes.py
@@ -7,19 +7,21 @@ def cycle():
         scenes = obs.obs_frontend_get_scenes()
         cycle_scenes = []
         for s in scenes:
-            if obs.obs_source_get_name(s).startswith('Cycle'):
+            if obs.obs_source_get_name(s).startswith('Cycle') and s != current_scene:
                 cycle_scenes.append(s)
-        scenes.remove(current_scene)
         obs.obs_frontend_set_current_scene(random.choice(cycle_scenes))
 
 def script_properties():
     props = obs.obs_properties_create()
 
+    obs.obs_properties_add_int(props, "enabled", "Enabled",
+                               0, 1, 0)
     obs.obs_properties_add_int(props, "cycle_rate", "Cycle Rate(ms)",
                                5000, 1000000, 5000)
     return props
 
 def script_update(settings):
     obs.timer_remove(cycle)
-    blink_rate = obs.obs_data_get_int(settings, "cycle_rate")
-    obs.timer_add(cycle, blink_rate)
+    if obs.obs_data_get_int(settings, "enabled"):
+        blink_rate = obs.obs_data_get_int(settings, "cycle_rate")
+        obs.timer_add(cycle, blink_rate)
